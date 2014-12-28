fffcfe9b27aab8277cc8d263d5096999510b8d3a
Brian Mullan <bmullan91@gmail.com>
2014-12-28 23:31:28 +0000

    Adding parameter tests

---
+++ test/parameter/test.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+var tv4 = require('tv4');
+var schema = require('../../schemas/header.json');
+
+describe('Parameter tests', function() {
+
+  it('should should accept valid json', function() {
+    expect(tv4.validate(require('./valid.json'), schema)).to.be.true;
+  });
+
+  it('should should reject invalid json', function() {
+    expect(tv4.validate(require('./invalid.json'), schema)).to.be.false;
+  });
+
+});
